e2e4dd3c7cade016070c913a6956a1b98a98fa9e
Anarion <marcodb.debona@gmail.com>
2020-10-11 10:16:35 +0200

    update request and rework create/fetch

--- src/index.js
+++ src/index.js
@@ -11,7 +11,8 @@ const app = express();
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useFindAndModify: false
 }).then(_ => console.log('connected to MongoDb'))
   .catch(err => console.error(err));
 
--- src/routes/event.js
+++ src/routes/event.js
@@ -6,21 +6,29 @@ const router = new express.Router();
 
 router.get('/', async (req, res) => {
   try {
-    res.send(await Event.find({}));
+    const listEvents = await Event.find({});
+    res.status(200).json(listEvents);
   } catch(err) {
-    console.error(err);
+    res.status(500).json(err);
   }
 });
 
 router.post('/', async (req, res) => {
   try {
-    const event = new Event({
-      ...req.body,
-      date: new Date()
-    });
-    res.send(await event.save());
+    const newEvent = (new Event(req.body));
+    newEvent.save();
+    res.status(200).json(newEvent);
   } catch(err) {
-    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedEvent = await Event.findByIdAndUpdate(req.params.id, req.body, {new: true});
+    res.status(200).json(updatedEvent);
+  } catch(err) {
+    res.status(500).json(err);
   }
 });
 
