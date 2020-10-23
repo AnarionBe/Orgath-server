746e57a0f98a9462af365c652ab1291c0cef968c
Anarion <marcodb.debona@gmail.com>
2020-10-23 23:20:52 +0200

    heroku port

--- src/index.js
+++ src/index.js
@@ -32,6 +32,6 @@ app.get('/', (req, res) => {
   res.send('Hello world');
 });
 
-app.listen(8000, () => {
-  console.log('Server running on port 8000');
+app.listen(process.env.PORT || 8000, () => {
+  console.log('Server running...');
 });
