117f6f1dea3718caa02014043356cdf16b9ee185
Anarion <marcodb.debona@gmail.com>
2020-10-24 20:17:14 +0200

    tests nested repo action

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ app.use('/event', eventRouter);
 app.use('/todo', todoRouter);
 
 app.get('/', (req, res) => {
-  res.send('Hello Heroku world!');
+  res.sendFile('./public/index.html');
 });
 
 app.listen(process.env.PORT || 8000, () => {
