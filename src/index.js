4f32968433cb3a366fd20c141264990ea0bcd145
Anarion <marcodb.debona@gmail.com>
2020-10-24 20:24:34 +0200

    fix path

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ app.use('/event', eventRouter);
 app.use('/todo', todoRouter);
 
 app.get('/', (req, res) => {
-  res.sendFile('./public/index.html');
+  res.sendFile('/public/index.html');
 });
 
 app.listen(process.env.PORT || 8000, () => {
