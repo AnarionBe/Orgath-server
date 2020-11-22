6eabadc9659c53fdcab4ab0b2e2abe00cf0fa0d1
Anarion <marcodb.debona@gmail.com>
2020-11-22 18:48:13 +0100

    update todo

--- src/routes/todo.js
+++ src/routes/todo.js
@@ -5,7 +5,7 @@ const router = new express.Router();
 
 router.get('/', async (req, res) => {
   try {
-    const listTodos = await Todo.find(req.query);
+    const listTodos = await Todo.find({...req.query, owner: req.token.id});
     return res.status(200).json(listTodos);
   } catch(err) {
     res.status(500).json(err);
@@ -14,7 +14,7 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   try {
-    const todo = await Todo.findById(req.params.id);
+    const todo = await Todo.findOne({_id: req.params.id, owner: req.token.id});
     return res.status(200).json(todo);
   } catch(err) {
     return res.status(500).json(err);
@@ -23,7 +23,7 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   try {
-    const newTodo = await new Todo(req.body);
+    const newTodo = await new Todo({...req.body, owner: req.token.id});
     await newTodo.save();
     return res.status(201).json(newTodo);
   } catch(err) {
@@ -33,7 +33,7 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
   try {
-    const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, req.body, {new: true});
+    const updatedTodo = await Todo.findOneAndUpdate({_id: req.params.id, owner: req.token.id}, req.body, {new: true});
     return res.status(200).json(updatedTodo);
   } catch(err) {
     return res.status(500).json(err);
@@ -42,7 +42,7 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   try {
-    const deletedTodo = await Todo.findByIdAndDelete(req.params.id);
+    const deletedTodo = await Todo.findOneAndDelete({_id: req.params.id, owner: req.token.id});
     return res.status(200).json(deletedTodo);
   } catch(err) {
     return req.status(500).json(err);
